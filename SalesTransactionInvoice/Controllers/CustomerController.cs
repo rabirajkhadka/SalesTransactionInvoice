14a7358a1f811cf47033fb091aec941ec30ff7a0
Rabi Raj Khadka <er.rabirajkhadka@gmail.com>
2017-06-06 17:58:19 +0545

    Customer List View Added

---
+++ SalesTransactionInvoice/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesTransactionInvoice.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Get Customers Details
+        public ActionResult GetCustomer()
+        {
+            return View();
+        }
+
+        //REtrieve data from Jquery
+        public JsonResult GetAllData()
+        {
+            List<Customer> allCustomer = new List<Customer>();
+
+            using (InvoicedBEntities idbc = new InvoicedBEntities())
+            {
+                allCustomer = idbc.Customers.ToList();
+
+            }
+            return new JsonResult { Data = allCustomer, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+        }
+
+        //REtrieve data with parameter from Jquery
+        public JsonResult GetCustomerDatawithparameter(string prefix)
+        {
+            List<Customer> allCustomer = new List<Customer>();
+
+            using (InvoicedBEntities idbc = new InvoicedBEntities())
+            {
+                allCustomer = idbc.Customers.Where(name=>name.Customer_NAME.Contains(prefix)).ToList();
+
+            }
+            return new JsonResult { Data = allCustomer, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+        }
+
+    }
+}
